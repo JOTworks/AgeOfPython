17903d8a25780d6bc1b55333a3897da5a9222346
Jackson Hunt <jhunt@4gclinical.com>
2025-05-13 20:49:41 -0400

    temp magic number

--- src/Printer.py
+++ src/Printer.py
@@ -138,6 +138,9 @@ class DefRulePrintVisitor(ast.NodeVisitor):
                         else:
                             next_expr_str = str(next_expr.id)
                         logger.warning(f"TypeOp missmatch {expr.string} {next_expr_str}, line {next_expr.end_lineno}")
+                
+                if next_expr_str == "15900": #todo: get rid of this magic number
+                    return expr.string
                 return prefix.string
             return prefix.string + expr.string
         
